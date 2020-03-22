3af373da2f0c7ecdaedbaa57ead5efba74a88aa4
swswl <62515384+swswl@users.noreply.github.com>
2020-03-22 13:52:37 -0400

    Add files via upload

---
+++ examples/wave_example.ino
@@ -0,0 +1,28 @@
+#define Pin 5 //PWM pin for output signal
+#include "wave.h"
+
+byte brightness = 255;
+Wave wave(Pin, 100);
+unsigned int duration;
+unsigned int animation;
+unsigned long start;
+
+void setup() {
+  // put your setup code here, to run once:
+  randomSeed(analogRead(0));
+  pinMode(Pin, OUTPUT);
+  unsigned long now = millis();
+  start = millis();
+}
+
+void loop() {
+  if (millis() - wave.getStart() < duration) {
+    wave.runWave();
+  } else {
+    // Setting up new animation
+    animation = random(0, 17);
+    int speed = random(500, 1300);
+    int rep = random(15, 15);
+    duration = wave.setWave(animation, speed, rep, brightness);
+  }
+}
